e5f07d1a48dff64ee7b7963d167ae58d0ea1cd03
Гаяне <gayanev.stepanyan@yandex.ru>
2023-06-30 00:45:01 +0300

    Migrations. Methods POST, GET are working

--- TodoList_Back/todos/serializers.py
+++ TodoList_Back/todos/serializers.py
@@ -1,28 +1,8 @@
 from rest_framework import serializers
-from todos.models import Todo, LANGUAGE_CHOICES, STYLE_CHOICES
+from todos.models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
-    
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Todo.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
-
-
+        fields = ['value', 'checked', 'code',]
--- TodoList_Back/todos/views.py
+++ TodoList_Back/todos/views.py
@@ -1,29 +1,7 @@
-from todos.models import Todo
-from todos.serializers import TodosSerializer
-from rest_framework import generics
-from django.contrib.auth.models import User
-from rest_framework import permissions
-from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from rest_framework import renderers
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import permissions
+from todos.models import Todo
+from todos.serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list`, `create`, `retrieve`,
-    `update` and `destroy` actions.
-
-    Additionally we also provide an extra `highlight` action.
-    """
     queryset = Todo.objects.all()
-    serializer_class = TodosSerializer
-
-
-    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    serializer_class = TodoSerializer
